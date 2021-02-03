4cea168730f2acda995f702bb0667b3075ef6a83
Abdullah <7589441@gmail.com>
2021-02-03 20:49:05 +0300

    Removed config loading

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import Server from "./core/server.core";
-import { RocklenAPIServerConfig } from "./types";
-import YAML from "yaml";
-import fs from "fs";
-import path from "path";
 
-let data: string = fs.readFileSync(path.join(__dirname, "..", "config.yaml"), "utf-8");
-let config: RocklenAPIServerConfig = YAML.parse(data);
-
-
-let server = new Server(config);
+let server = new Server();
 
 server.connect().then(() => {
   server.build().launch();
