1a00ca2a506d687268dfcf8c8a980505ab5e7c9c
Abdullah <7589441@gmail.com>
2021-02-03 23:49:57 +0300

    Added jwt auth method, so the failure message can be customizable

--- src/core/util.core.ts
+++ src/core/util.core.ts
@@ -1,6 +1,9 @@
 import RocklenAPIServer from "./server.core";
 import JWT from "jsonwebtoken";
 
+import { Request, Response, NextFunction } from "express";
+import passport from "passport";
+
 class RocklenAPIServerUtil {
   public server: RocklenAPIServer
   public constructor(server: RocklenAPIServer) {
@@ -15,6 +18,20 @@ class RocklenAPIServerUtil {
     }, process.env.ACCESS_TOKEN_SECRET as string, {
       expiresIn: "1w"
     });
+  }
+  
+  public static authJWT(req: Request, res: Response, next: NextFunction) {
+    passport.authenticate("jwt", {
+      session: false
+    }, (err, user) => {
+      if(err) return next(err);
+      if(!user) {
+        return res.status(401).send({
+          message: "FAILED_TO_AUTHORIZE",
+          code: 401
+        });
+      }
+    })(req, res, next);
   }
 };
 
