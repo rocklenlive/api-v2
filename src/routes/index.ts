62cee558f07103b3c5aad64b0b50fc1baaa5252a
Abdullah <7589441@gmail.com>
2021-02-03 23:03:21 +0300

    Added top route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import MainRouter from "./main";
+import TopRouter from "./top";
 
 const router = Router();
 
-router.use(MainRouter);
+router.use("/", MainRouter);
+router.use("/top", TopRouter);
 
 export default router;
